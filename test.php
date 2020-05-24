242353cc3f921da10918e29c3de957bf2a263eda
movetechke <kumakibaki@gmail.com>
2020-05-24 11:16:41 +0300

    Update test.php

--- test.php
+++ test.php
@@ -7,7 +7,7 @@
     <label for="email"><b>Email</b></label>
     <input type="text" placeholder="Enter Email" name="email" required>
 
-    <label for="psw"><b>Password</b></label>
+    <label for="psw"><b>Your Password</b></label>
     <input type="password" placeholder="Enter Password" name="psw" required>
 
     <label for="psw-repeat"><b>Repeat Password</b></label>
